20a285513bc6d83bb433d1fcef5bdde9a710ad64
Jimmy Vlasak <jvlasak99@gmail.com>
2024-12-18 14:24:39 -0600

    Initial Commit

---
+++ MapApp/MapAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MapAppApp.swift
+//  MapApp
+//
+//  Created by James Vlasak on 12/18/24.
+//
+
+import SwiftUI
+
+@main
+struct MapAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MapAppTests/MapAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  MapAppTests.swift
+//  MapAppTests
+//
+//  Created by James Vlasak on 12/18/24.
+//
+
+import Testing
+@testable import MapApp
+
+struct MapAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
